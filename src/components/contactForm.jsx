7131fba2fbdc0afd397f686faf79e05d6322631a
Javi <nb2javi@gmail.com>
2023-11-03 15:12:44 -0500

    Installed formspree package and created contactform component for contact page

---
+++ src/components/contactForm.jsx
@@ -0,0 +1,31 @@
+import { useForm, ValidationError } from "@formspree/react";
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xqknoyrn");
+  if (state.succeeded) {
+    return <p>Thanks for reaching out!</p>;
+  }
+  return (
+    <form className="form-object" onSubmit={handleSubmit}>
+      <input
+        className="form-email"
+        placeholder="Your Email"
+        id="email"
+        type="email"
+        name="email"
+      />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <textarea
+        className="form-body"
+        placeholder="Your Message"
+        id="message"
+        name="message"
+      />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <button className="button" type="submit" disabled={state.submitting}>
+        Send
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,10 @@
+import ContactForm from "../components/contactForm";
+
 export default function Contact() {
   return (
     <div>
-      <h2>Test Contact</h2>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci sequi
-        officia magnam enim magni accusamus maxime aliquam sit aut dolorem harum
-        neque beatae iure amet sint distinctio reiciendis veniam, ipsum hic
-        veritatis voluptas similique deserunt aliquid! Repellat eius nesciunt
-        possimus quibusdam laudantium, distinctio blanditiis consectetur! Maxime
-        fuga quae quasi doloribus.
-      </p>
+      <h2>Reach out to me!</h2>
+      <ContactForm />
     </div>
   );
 }
