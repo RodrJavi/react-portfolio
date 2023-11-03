20bf957e23aade73fee1832912328c62bf0a58bb
Javi <nb2javi@gmail.com>
2023-11-03 13:09:35 -0500

    Displaying pages and have router functioning

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./index.css";
+
+import App from "./App.jsx";
+import About from "./pages/About.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
+import Contact from "./pages/Contact.jsx";
+import Resume from "./pages/Resume.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "/About",
+        element: <About />,
+      },
+      {
+        path: "/Portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+      {
+        path: "/Resume",
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,15 @@
+export default function Portfolio() {
+  return (
+    <div>
+      <h2>Test</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci sequi
+        officia magnam enim magni accusamus maxime aliquam sit aut dolorem harum
+        neque beatae iure amet sint distinctio reiciendis veniam, ipsum hic
+        veritatis voluptas similique deserunt aliquid! Repellat eius nesciunt
+        possimus quibusdam laudantium, distinctio blanditiis consectetur! Maxime
+        fuga quae quasi doloribus.
+      </p>
+    </div>
+  );
+}
