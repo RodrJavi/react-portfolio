b50444ce0623224595cea2fc4ad83bb2eb5bf1a9
Javi <nb2javi@gmail.com>
2023-11-03 13:25:37 -0500

    structured navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <ul>
+      <li>
+        <Link to="/About">About</Link>
+      </li>
+      <li>
+        <Link to="/Contact">Contact</Link>
+      </li>
+      <li>
+        <Link to="/Portfolio">Portfolio</Link>
+      </li>
+      <li>
+        <Link to="/Resume">Resume</Link>
+      </li>
+    </ul>
+  );
+}
